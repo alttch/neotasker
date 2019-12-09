8c363ff513afcee3ff77d550ff38846948afa1d5
Sergei S <div@altertech.com>
2019-12-09 21:22:06 +0000

    readme and setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+__version__ = '0.0.1'
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='neotasker',
+    version=__version__,
+    author='Altertech',
+    author_email='div@altertech.com',
+    description=
+    'Lightweight thread and asyncio task library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alttch/neotasker',
+    packages=setuptools.find_packages(),
+    license='Apache License 2.0',
+    install_requires=['aiosched'],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT',
+        'Topic :: Software Development :: Libraries',
+    ),
+)
