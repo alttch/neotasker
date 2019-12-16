4fbc046020037cf77f6065d924464f46972060ac
Sergei S <div@altertech.com>
2019-12-16 20:42:11 +0000

    0.0.20

--- neotasker/__init__.py
+++ neotasker/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'Altertech, https://www.altertech.com/'
 __copyright__ = 'Copyright (c) 2019 Altertech'
 __license__ = 'MIT'
-__version__ = '0.0.19'
+__version__ = '0.0.20'
 
 from neotasker.supervisor import TaskSupervisor
 
